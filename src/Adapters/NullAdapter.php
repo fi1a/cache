a261b1101548bbea8f7d6b14ed06515b2e5dd6e3
Nikolay Gorohovsky <fi1a@icloud.com>
2022-12-02 05:44:18 +0500

    Null adapter

---
+++ src/Adapters/NullAdapter.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fi1a\Cache\Adapters;
+
+/**
+ * Адаптер кэширования (Null объект)
+ */
+class NullAdapter implements AdapterInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function fetch(array $keys): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function save(array $values): bool
+    {
+        return true;
+    }
+
+    /**
+    * @inheritDoc
+    */
+    public function have(string $key, string $namespace, ?string $hash = null): bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete(array $keys, string $namespace): bool
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function clear(): bool
+    {
+        return true;
+    }
+}
--- tests/Adapters/NullAdapterTest.php
+++ tests/Adapters/NullAdapterTest.php
@@ -4,41 +4,29 @@ declare(strict_types=1);
 
 namespace Fi1a\Unit\Cache\Adapters;
 
-use ErrorException;
 use Fi1a\Cache\Adapters\AdapterInterface;
-use Fi1a\Cache\Adapters\FilesystemAdapter;
-use Fi1a\Unit\Cache\TestCase\FilesystemAdapterTestCase;
-use InvalidArgumentException;
+use Fi1a\Cache\Adapters\NullAdapter;
+use PHPUnit\Framework\TestCase;
 
 /**
- * Адаптер кэширования в файловой системе
+ * Адаптер кэширования (Null объект)
  */
-class FilesystemAdapterTest extends FilesystemAdapterTestCase
+class NullAdapterTest extends TestCase
 {
     /**
      * Возвращает адаптер
      */
     private function getAdapter(): AdapterInterface
     {
-        return new FilesystemAdapter(self::$folderPath . '/cache');
+        return new NullAdapter();
     }
 
     /**
-     * Инициализация кеша
+     * Конструктор
      */
-    public function testConstructorException()
+    public function testConstruct(): void
     {
-        $this->expectException(ErrorException::class);
-        mkdir(self::$folderPath, 0775, true);
-        chmod(self::$folderPath, 0000);
-        try {
-            new FilesystemAdapter(self::$folderPath . '/cache');
-        } catch (ErrorException $exception) {
-            chmod(self::$folderPath, 0775);
-            self::deleteCacheFolder();
-
-            throw $exception;
-        }
+        $this->assertInstanceOf(AdapterInterface::class, $this->getAdapter());
     }
 
     /**
@@ -57,21 +45,6 @@ class FilesystemAdapterTest extends FilesystemAdapterTestCase
     }
 
     /**
-     * Сохранение значений в кэше
-     */
-    public function testSaveError(): void
-    {
-        $adapter = $this->getAdapter();
-        chmod(self::$folderPath, 0000);
-        $this->assertFalse($adapter->save([
-            'key1' => ['value1', 'hash1', time() + 1000, ''],
-            'key2' => ['value2', 'hash2', time() + 1000, ''],
-            'key3' => ['value3', null, time() + 1000, ''],
-        ]));
-        chmod(self::$folderPath, 0775);
-    }
-
-    /**
      * Проверяет наличие значения в кэше
      *
      * @depends testSave
@@ -79,10 +52,10 @@ class FilesystemAdapterTest extends FilesystemAdapterTestCase
     public function testHave(): void
     {
         $adapter = $this->getAdapter();
-        $this->assertTrue($adapter->have('key1', '', 'hash1'));
+        $this->assertFalse($adapter->have('key1', '', 'hash1'));
         $this->assertFalse($adapter->have('unknown', ''));
-        $this->assertTrue($adapter->have('key4', 'some-namespace', 'hash4'));
-        $this->assertTrue($adapter->have('key5', 'some/namespace', 'hash5'));
+        $this->assertFalse($adapter->have('key4', 'some-namespace', 'hash4'));
+        $this->assertFalse($adapter->have('key5', 'some/namespace', 'hash5'));
         $this->assertFalse($adapter->have('key1', 'some-namespace', 'hash1'));
     }
 
@@ -94,7 +67,7 @@ class FilesystemAdapterTest extends FilesystemAdapterTestCase
     public function testFetch(): void
     {
         $adapter = $this->getAdapter();
-        $this->assertCount(2, $adapter->fetch([
+        $this->assertCount(0, $adapter->fetch([
             ['key1', 'hash1', ''],
             ['key2', 'new-hash', ''],
             ['key3', null, ''],
@@ -123,35 +96,10 @@ class FilesystemAdapterTest extends FilesystemAdapterTestCase
      *
      * @depends testSave
      */
-    public function testClearFail(): void
-    {
-        $adapter = $this->getAdapter();
-        chmod(self::$folderPath, 0000);
-        $this->assertFalse($adapter->clear());
-        chmod(self::$folderPath, 0775);
-    }
-
-    /**
-     * Очищает кэш
-     *
-     * @depends testSave
-     */
     public function testClear(): void
     {
         $adapter = $this->getAdapter();
         $this->assertTrue($adapter->clear());
         $this->assertTrue($adapter->clear());
-    }
-
-    /**
-     * Использование namespace
-     */
-    public function testNamespaceException(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $adapter = $this->getAdapter();
-        $this->assertTrue($adapter->save([
-            'key3' => ['value3', null, time() + 1000, '@'],
-        ]));
     }
 }
